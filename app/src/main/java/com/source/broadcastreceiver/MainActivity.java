e487dd4d6abefb70248fcb1dc18ebeb4694cdf2c
assiatashu <assiatashu@github.com>
2018-02-11 12:01:50 +0530

    Initial commit

---
+++ app/src/main/java/com/source/broadcastreceiver/MainActivity.java
@@ -0,0 +1,41 @@
+package com.source.broadcastreceiver;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    ConnectionReceiver receiver;
+    IntentFilter intentFilter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        receiver = new ConnectionReceiver();
+        intentFilter = new IntentFilter("com.syntaxt.broadcastreceiver.SOME_ACTION");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(receiver, intentFilter);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(receiver);
+    }
+
+
+    public void someMethod(View view) {
+        Intent intent = new Intent("com.syntaxt.broadcastreceiver.SOME_ACTION");
+        sendBroadcast(intent);
+    }
+}
